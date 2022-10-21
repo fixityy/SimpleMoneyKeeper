20081137e5385fdf0131562ad44cf287ab448772
fixityy <belov.roman.yur@gmail.com>
2022-10-21 15:16:08 +0300

    Initial Commit

---
+++ SimpleMoneyKeeper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMoneyKeeper
+//
+//  Created by Roman Belov on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
