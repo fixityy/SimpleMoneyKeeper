52c49848ed58a28e76a141a1a4cd6dc0dd0523d6
fixityy <belov.roman.yur@gmail.com>
2022-11-09 18:01:43 +0300

    Refactoring

---
+++ SimpleMoneyKeeper/Main Module/Presenter/MainPresenter.swift
@@ -0,0 +1,24 @@
+//
+//  MainPresenter.swift
+//  SimpleMoneyKeeper
+//
+//  Created by Roman Belov on 09.11.2022.
+//
+
+import Foundation
+
+protocol FrontViewProtocol: AnyObject {
+    
+}
+
+protocol BackViewProtocol: AnyObject {
+    
+}
+
+protocol MainPresenterProtocol: AnyObject {
+    
+}
+
+class MainPresenter: MainPresenterProtocol {
+    
+}
--- SimpleMoneyKeeper/Main Module/View/BackViewController.swift
+++ SimpleMoneyKeeper/Main Module/View/BackViewController.swift
@@ -9,24 +9,34 @@ import UIKit
 
 class BackViewController: UIViewController {
     
+    var dates: [Date]!
     
-    lazy var dateAndChartCollectionView = DateAndChartCollectionView()
+    //MARK: temp, delete after
+    var decreaseMonth = -2
+    var increaseMonth = 2    
+    
+    lazy var dateAndChartCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = Colors.mainAccentColor
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(DateAndChartCollectionViewCollectionViewCell.self, forCellWithReuseIdentifier: DateAndChartCollectionViewCollectionViewCell.reuseID)
+        
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = Colors.mainAccentColor
-        
-        view.addSubview(dateAndChartCollectionView)
-        makeConstraint()
-        
-        let dates: [Date] = [addOrSubtractMonth(month: -1), addOrSubtractMonth(month: 0), addOrSubtractMonth(month: 1)]
-
-        dateAndChartCollectionView.dates = dates
-        
-        
-        
-//        test()
+        setupViews()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -36,54 +46,15 @@ class BackViewController: UIViewController {
 
     }
     
-    private func test() {
+    private func setupViews() {
+        view.backgroundColor = Colors.mainAccentColor
         
-        let date1 = Date(timeIntervalSinceNow: 0)
-        let date2 = Date(timeIntervalSinceNow: 41234123)
-        let date3 = Date(timeIntervalSinceNow: 1231232)
+        view.addSubview(dateAndChartCollectionView)
+        makeConstraint()
         
-        let dataFormatter = DateFormatter()
-        dataFormatter.dateFormat = "yyyy.MM"
-        
-        let d1 = dataFormatter.string(from: date1)
-        let d2 = dataFormatter.string(from: date2)
-        let d3 = dataFormatter.string(from: date3)
-        
-        let d4 = date1.formatted(.dateTime.month(.wide))
-        let d5 = date1.formatted(.dateTime.year(.defaultDigits).month(.defaultDigits))
-        let d6 = date2.formatted(.dateTime.year(.defaultDigits).month(.defaultDigits))
-        
-        let d7 = date1.formatted(date: .numeric, time: .omitted)
-        let d8 = date2.formatted(date: .numeric, time: .omitted)
-        let d9 = date3.formatted(date: .numeric, time: .omitted)
-
-        print(d4)
-        print(d5)
-        print(d6)
-
-        d5 == d6 ? print("true") : print("false")
-        
-        print(d1)
-        print(d2)
-        print(d3)
-        
-        let array = [d1, d2, d3]
-        let array2 = [date1, date2, date3]
-        let array3 = [d9, d8, d9, d7]
-        let array4 = [d5, d6, d6, d5]
-
-        print(array.sorted(by: < ))
-        print(array2.sorted(by: < ))
-        print(array3.sorted(by: <))
-        print(array4.sorted(by: <))
-        
-        d1 < d2 ? print("true") : print("false")
-        
-        
-        print(addOrSubtractMonth(month: 0))
-        print(addOrSubtractMonth(month: 2))
-        print(addOrSubtractMonth(month: -1))
+        dates = [addOrSubtractMonth(month: -1), addOrSubtractMonth(month: 0), addOrSubtractMonth(month: 1)]
     }
+
     
     //MARK: temp, delete after
     func addOrSubtractMonth(month: Int) -> Date {
@@ -100,6 +71,58 @@ class BackViewController: UIViewController {
         ])
         
     }
+}
 
+
+extension BackViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        dates.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = dateAndChartCollectionView.dequeueReusableCell(withReuseIdentifier: DateAndChartCollectionViewCollectionViewCell.reuseID, for: indexPath) as! DateAndChartCollectionViewCollectionViewCell
+        cell.dateLabel.text = dates[indexPath.row].formatted()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: dateAndChartCollectionView.frame.width, height: dateAndChartCollectionView.frame.height)
+    }
+    
+    //Adding next date to dataSource
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        guard let oldIndexPath = collectionView.indexPathsForVisibleItems.first else { return }
+
+        if oldIndexPath.row < indexPath.row && (dates.count - (oldIndexPath.row + 1)) == 1 {
+            dates.insert(addOrSubtractMonth(month: increaseMonth), at: dates.endIndex)
+            increaseMonth += 1
+            collectionView.reloadData()
+        }
+    }
+    
+    //Adding previous date to dataSource
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        guard let newIndexPath = collectionView.indexPathsForVisibleItems.first else { return }
+        
+        if newIndexPath.row < indexPath.row && newIndexPath.row == 0 {
+            
+            DispatchQueue.global().async { [weak self] in
+                guard let self = self else { return }
+                self.dates.insert(self.addOrSubtractMonth(month: self.decreaseMonth), at: 0)
+                self.decreaseMonth -= 1
+                
+                DispatchQueue.main.async {
+                    collectionView.reloadData()
+                    collectionView.scrollToItem(at: IndexPath(row: newIndexPath.row + 1, section: 0), at: .centeredHorizontally, animated: false)
+                }
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
 }
 
